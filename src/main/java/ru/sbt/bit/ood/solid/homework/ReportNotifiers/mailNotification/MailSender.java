958326aa6a1259d3daf418ddb62a4e0ce38b2089
Артем <artiom-sultan@yandex.ru>
2015-04-14 16:16:50 +0300

    no message

---
+++ src/main/java/ru/sbt/bit/ood/solid/homework/ReportNotifiers/mailNotification/MailSender.java
@@ -0,0 +1,8 @@
+package ru.sbt.bit.ood.solid.homework.reportNotifiers.mailNotification;
+
+/**
+ * Created by artem on 14.04.15.
+ */
+public interface MailSender {
+    void sendEMAil(EMailObject eMailObject);
+}
---
+++ src/main/java/ru/sbt/bit/ood/solid/homework/ReportNotifiers/mailNotification/ReportMailFactory.java
@@ -0,0 +1,12 @@
+package ru.sbt.bit.ood.solid.homework.reportNotifiers.mailNotification;
+
+/**
+ * Created by artem on 23.03.15.
+ */
+public class ReportMailFactory {
+    private static final String subject = "Monthly department salary report";
+
+    public static EMailObject createReportMail(String report, String recipients) {
+        return new EMailObject(subject,report,recipients,null);
+    }
+}
